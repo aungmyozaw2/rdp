d7af354bbe0bc055198477514e06b03ef1e24c13
Aung Myo Zaw <71240643+aungmyozaw2@users.noreply.github.com>
2021-08-22 08:15:52 +0630

    Create rdpmodifyname.sh

---
+++ rdpmodifyname.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+printf "This script modify by Aung Myo Zaw.This is not my script crd to owner wait for download \n " >&2
+{
+sudo useradd -m aunglay1
+sudo adduser aunglay1 sudo
+echo 'aunglay1:aunglay1' | sudo chpasswd
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+sudo apt-get update
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo DEBIAN_FRONTEND=noninteractive \
+apt install --assume-yes xfce4 desktop-base
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+sudo apt install --assume-yes xscreensaver
+sudo systemctl disable lightdm.service
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+wget https://media.codeweavers.com/pub/crossover/cxlinux/demo/crossover_20.0.2-1.deb
+sudo dpkg -i crossover_20.0.2-1.deb
+sudo apt install --assume-yes --fix-broken
+sudo apt install nautilus nano -y
+sudo apt install xfce4-terminal -y
+sudo apt -y install obs-studio
+sudo apt -y install firefox
+sudo adduser aunglay1 chrome-remote-desktop
+} &> /dev/null &&
+printf "\nSetup Completed " >&2 ||
+printf "\nError Occured " >&2
+printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
+read -p "Paste Here: " CRP
+su - aunglay1 -c """$CRP"""
+printf 'Check https://remotedesktop.google.com/access/ \n'
+printf 'Your SUDO Pasword Is aunglay1 \n'
+printf 'This is my account www.facebook.com/aungmyozaw123 \n'
